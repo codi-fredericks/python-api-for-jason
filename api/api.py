e227a09dc87bec20c8fea68c198f3a34070cc6d2
DAVE <68131899+codi-hub@users.noreply.github.com>
2024-07-04 00:43:05 +1200

    Match data for user id

--- api/api.py
+++ api/api.py
@@ -2,17 +2,17 @@ import requests
 import urllib.parse
 
 base_url = "https://api.apacbreachersranked.com/"
-#                                            
-#    ,ad8888ba,   88888888888  888888888888  
-#   d8"'    `"8b  88                88       
-#  d8'            88                88       
-#  88             88aaaaa           88       
-#  88      88888  88"""""           88       
-#  Y8,        88  88                88       
-#   Y8a.    .a88  88                88       
-#    `"Y88888P"   88888888888       88       
-#                                            
-#                                            
+#                                                       
+#  88        88   ad88888ba   88888888888  88888888ba   
+#  88        88  d8"     "8b  88           88      "8b  
+#  88        88  Y8,          88           88      ,8P  
+#  88        88  `Y8aaaaa,    88aaaaa      88aaaaaa8P'  
+#  88        88    `"""""8b,  88"""""      88""""88'    
+#  88        88          `8b  88           88    `8b    
+#  Y8a.    .a8P  Y8a     a8P  88           88     `8b   
+#   `"Y8888Y"'    "Y88888P"   88888888888  88      `8b  
+#                                                       
+#                                                                                         
 
 class BreachersUser:
     def __init__(self, id, userName, clanTag, fullUserName):
@@ -54,18 +54,122 @@ def get_user(BreachersUserId, json=False):
     else:
         raise ValueError(f"Request error {result.status_code}")
 
-def get_user_match(BreachersUserId, MatchNum, json=False):
+#                                                                                                    
+#  88b           d88         db    888888888888  ,ad8888ba,   88        88  88888888888  ad88888ba   
+#  888b         d888        d88b        88      d8"'    `"8b  88        88  88          d8"     "8b  
+#  88`8b       d8'88       d8'`8b       88     d8'            88        88  88          Y8,          
+#  88 `8b     d8' 88      d8'  `8b      88     88             88aaaaaaaa88  88aaaaa     `Y8aaaaa,    
+#  88  `8b   d8'  88     d8YaaaaY8b     88     88             88""""""""88  88"""""       `"""""8b,  
+#  88   `8b d8'   88    d8""""""""8b    88     Y8,            88        88  88                  `8b  
+#  88    `888'    88   d8'        `8b   88      Y8a.    .a8P  88        88  88          Y8a     a8P  
+#  88     `8'     88  d8'          `8b  88       `"Y8888Y"'   88        88  88888888888  "Y88888P"   
+#                                                                                                    
+#                                                                                                    
+
+class Match():
+    def __init__(self, id, players, timestamp, game_data):
+        self.id = id
+        self.players = players
+        self.timestamp = timestamp #UTC
+        self.game_data = self.MatchData(**game_data)
+    
+    class MatchData():
+        def __init__(self, MapName, matchMode ,matchType, players, playersLeft, allPlayers):
+            self.MapName = MapName
+            self.matchMode = matchMode
+            self.matchType = matchType
+            self.players = []
+            for player in players:
+                self.players.append(self.MatchPlayer(**player))
+            self.playersLeft = []
+            for player in playersLeft:
+                self.playersLeft.append(self.MatchPlayer(**player))
+            self.allPlayers = []
+            for player in allPlayers:
+                self.allPlayers.append(self.MatchPlayer(**player))
+        
+        class MatchPlayer():
+            def __init__(self, ApiId, clanTag, GameMatchDataResult, score, gameTimeInSeconds, gameTime, mvp, penaltyReason, rounds, timeAfk, Username):
+                self.ApiId = ApiId
+                self.clanTag = clanTag
+                self.GameMatchDataResult = GameMatchDataResult
+                self.score = score
+                self.gameTimeInSeconds = gameTimeInSeconds
+                self.gameTime = gameTime
+                self.mvp = mvp
+                self.penaltyReason = penaltyReason
+                self.rounds = []
+                for round in rounds:
+                    self.rounds.append(self.MatchPlayerRound(**round))
+                self.timeAfk = timeAfk
+                self.Username = Username
+            
+            class MatchPlayerRound():
+                def __init__(self, ace, assists, botAssists, deaths, firstBlood, mvp, roundNumber, roundTime, team, teamWon, gadgets, weapons):
+                    self.ace = ace
+                    self.assists = assists
+                    self.botAssists = botAssists
+                    self.deaths = deaths
+                    self.firstBlood = firstBlood
+                    self.mvp = mvp
+                    self.roundNumber = roundNumber
+                    self.roundTime = roundTime
+                    self.team = team
+                    self.teamWon = teamWon
+                    self.gadgets = []
+                    for gadget in gadgets:
+                        self.gadgets.append(Gadget(**gadget))
+                    self.weapons = []
+                    for weapon in weapons:
+                        self.weapons.append(Weapon(**weapon))
+
+def get_user_match(BreachersUserId, json=False):
     '''
     `BreachersUserId` The id of a breachers user\n
-    `MatchNum` the match number you want to get `-1` for all\n
+    `MatchNum` the match number (0-9) you want to get `-1` for all matches\n
     `json` if True return user as json otherwise returns `BreachersUser` object
     '''
     encoded_id = urllib.parse.quote(BreachersUserId)
-    result = requests.get(f'{base_url}MatchStats/CsvMatchData?BreachersPlayerId='+encoded_id)
+    result = requests.get(f'{base_url}BreachersApi/get_match_data?player_id='+encoded_id)
+    print(result.status_code)
     if result.ok:
         if json:
-            return result.text
+            return result.json()
         else:
-            return BreachersUser(**result.json())
+            matches = []
+            for i in result.json():
+                matches.append(Match(**i))
+            return matches
     else:
         raise ValueError(f"Request error {result.status_code}")
+    
+
+
+
+class Gadget():
+    def __init__(self, Name, botDamageDone, botKills, damageDone, destroyed, enemyTriggered, friendlyDamageDone, kills, teamHealed, triggered, used):
+        self.Name = Name
+        self.botDamageDone = botDamageDone
+        self.botKills = botKills
+        self.damageDone = damageDone
+        self.destroyed = destroyed
+        self.enemyTriggered = enemyTriggered
+        self.friendlyDamageDone = friendlyDamageDone
+        self.kills = kills
+        self.teamHealed = teamHealed
+        self.triggered = triggered
+        self.used = used
+
+class Weapon():
+    def __init__(self, Name, botDamageDone, botHeadshotKills, botKills, damageDone, friendlyDamageDone, headshotKills, shotsFired, totalHeadshots, totalKills, totalShotsHit):
+        self.Name = Name
+        self.botDamageDone = botDamageDone
+        self.botHeadshotKills = botHeadshotKills
+        self.botKills = botKills
+        self.damageDone = damageDone
+        self.friendlyDamageDone = friendlyDamageDone
+        self.headshotKills = headshotKills
+        self.shotsFired = shotsFired
+        self.totalHeadshots = totalHeadshots
+        self.totalKills = totalKills
+        self.totalShotsHit = totalShotsHit
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from api import api
-
+import json
 
 
 x = input("username:\n>>")
@@ -7,11 +7,30 @@ users = api.search_users(x)
 
 for user in users:
     print(f"({user.id}) #{user.clanTag} {user.userName}\n")
-
+'''
 x = input("ID:\n>>")
 query = api.get_user(x)
 print(f"({query.id}) #{query.clanTag} {query.userName}\n")
+'''
 
+'''
+#JSON Output
 x = input("ID:\n>>")
-query = api.get_user_match(x, 0, True)
-print(query)
+query = api.get_user_match(x, True)
+match_num = int(input("Match Number\n>>"))
+with open(f'data/{(api.get_user(x).userName)}_data.json', 'w', encoding='utf-8') as f:
+    json.dump(query[match_num], f, ensure_ascii=False, indent=4)
+'''
+
+#Object Output
+x = input("ID:\n>>")
+query = api.get_user_match(x, False)
+match_num = int(input("Match Number\n>>"))
+match = query[match_num]
+
+for player in match.game_data.players:
+    if player.ApiId == x:
+        print(f"Score: {player.score}\nMVP: {player.mvp}\n")
+        for round in player.rounds:
+            print(f"Round: {round.roundNumber}\nSide: {round.team}\nMVP: {round.mvp}\nAce: {round.ace}\nDeaths: {round.deaths}")
+            print("-"*25)
